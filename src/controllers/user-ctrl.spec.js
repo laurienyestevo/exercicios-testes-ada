cc83c3771d2fe83994139798e25687922592ad40
Lucas Machado <machado051@gmail.com>
2024-02-04 17:33:41 -0300

    refactor: add test session-crtl and update user-crtl-scpec (#3)

--- src/controllers/user-ctrl.spec.js
+++ src/controllers/user-ctrl.spec.js
@@ -1,12 +1,3 @@
-// user-ctrl
-// verificar se existe requisição e resposta, especificando os campos do req.body
-// Verificar Email
-// Se foi preenchido, senão, ele retorna um erro 400 --> is not value
-// Status 200 se válido.
-// Verificar senha (Error e Sucesso)
-// Se a senha existe, ou seja, not null
-// se a senha é válida. Obs: Lembrar que o ID Existe
-
 const UserController = require("./user-ctrl");
 const UserService = require("../services/user-service");
 const Email = require("../utils/email-validator");
@@ -20,22 +11,21 @@ describe("UserController", () => {
                 password: "password123",
             },
         };
-        //vamos simular uma resposta http com o jest,
         const res = {
-            status: jest.fn().mockReturnThis(), //simula uma função para retornar o res
-            json: jest.fn(), //verifica se a res.json foi chamada certinho
+            status: jest.fn().mockReturnThis(), 
+            json: jest.fn(), 
         };
+
         const createUserMock = jest
             .spyOn(UserService, "createUser")
-            .mockResolvedValue({ id: "123" }); //o spyon é tipo um espiao que vai verificar quantas vezes a função foi chamada e quais argumentos foram passados
-        //criamos um espiao para o metodo createUser do objeto UserService, e passamos um valor que ele vai retornar, no caso um objeto com um id
+            .mockResolvedValue({ id: "123" });
         await UserController.create(req, res);
 
         expect(createUserMock).toHaveBeenCalledWith({
             name: "John Doe",
             email: "johndoe@example.com",
             password: "password123",
-        }); //aqui a gente garante que o metodo createUserMock foi chamado com os argumentos corretos
+        }); 
         expect(res.status).toHaveBeenCalledWith(200);
         expect(res.json).toHaveBeenCalledWith({ id: "123" });
     });
@@ -53,10 +43,10 @@ describe("UserController", () => {
             json: jest.fn(),
         };
 
-        // Use o método isValid da classe Email para verificar se o email é válido
+       
         const isValid = Email.isValid(req.body.email);
 
-        // Se o email não for válido, retorne um status 400 e uma mensagem de erro
+        
         if (!isValid) {
             res.status(400);
             res.json({ message: "Email inválido" });
@@ -68,39 +58,13 @@ describe("UserController", () => {
         expect(res.json).toHaveBeenCalledWith({ message: "Email inválido" });
     });
 
-    it("Deve retornar o status 400 e uma mensagem de erro caso a senha seja inválida", async () => {
-        const req = {
-            body: {
-                name: "John Doe",
-                email: "invalidemail",
-                password: "",
-            },
-        };
-        const res = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn(),
-        };
 
-        const isValid = Email.isValid(req.body.email);
-
-        // Se o email não for válido, retorne um status 400 e uma mensagem de erro
-        if (!isValid) {
-            res.status(400);
-            res.json({ message: "Email inválido" });
-        }
-
-        await UserController.create(req, res);
-
-        expect(res.status).toHaveBeenCalledWith(400);
-        expect(res.json).toHaveBeenCalledWith({ message: "Email inválido" });
-    });
-
-    it("Deve retornar o status 400 e uma mensagem de erro caso a senha for inválida", async () => {
+    it("Deve retornar o status 400 e uma mensagem de erro caso a senha for inválida", async () => { 
         const req = {
             body: {
                 name: "John Doe",
                 email: "johndoe@example.com",
-                password: "", // Senha inválida
+                password: "",
             },
         };
         const res = {
@@ -153,7 +117,6 @@ describe("UserController", () => {
         expect(res.json).toHaveBeenCalledWith("Server Error");
     });
 });
-//validar a senha
 
 describe("changePassword", () => {
     it("deve registrar uma mensagem e retornar o status 200", () => {
